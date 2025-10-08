4423a50c8468b942b7ebc3dd2f2bb48bb22aa606
MGC55 <mariogc5555@gmail.com>
2025-10-07 22:30:09 -0500

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,25 +4,17 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middlewares/authMiddleware');
 
-// --- Rutas Públicas (Registro y Login) ---
-
-// POST /api/users/register - Registrar un nuevo usuario (Ya existe)
 router.post('/register', userController.guardarUsuario);
 
-// POST /api/users/login - Iniciar sesión y obtener un token JWT
 router.post('/login', userController.loginUsuario); 
 
-
-// --- Rutas Protegidas ---
-
-// Nueva ruta para obtener el perfil del usuario logueado
 router.get('/me', auth.verificarToken, userController.obtenerPerfilUsuario);
 
-// GET /api/users - Obtener todos los usuarios
 router.get('/', 
-    auth.verificarToken,            // 1. Debe tener un token válido
-    auth.verificarRol('admin'),     // 2. Debe tener el rol de 'admin'
+    auth.verificarToken,
+    auth.verificarRol('admin'),
     userController.obtenerUsuarios
 );
 
 module.exports = router;
+
