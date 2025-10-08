4f75341f382cd46a9c877df2c78a20bf6a280f3a
MGC55 <mariogc5555@gmail.com>
2025-10-07 22:28:37 -0500

    Update projectRoutes.js

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -2,25 +2,19 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
-const auth = require('../middlewares/authMiddleware'); // Middleware de seguridad
+const auth = require('../middlewares/authMiddleware');
 
-const adminRol = auth.verificarRol('admin'); // Alias para la verificación de admin
+const adminRol = auth.verificarRol('admin');
 
-// Rutas de Proyectos
-
-// Crear un nuevo proyecto (Solo Admin)
 router.post('/', auth.verificarToken, adminRol, projectController.postCrearProyecto);
 
-// Obtener todos los proyectos (Solo Admin por ahora, podría cambiarse a Miembros)
 router.get('/', auth.verificarToken, adminRol, projectController.getProyectos);
 
-// Obtener un proyecto específico por ID (Solo Admin)
 router.get('/:id', auth.verificarToken, adminRol, projectController.getProyectoPorId);
 
-// Actualizar un proyecto (Solo Admin)
 router.put('/:id', auth.verificarToken, adminRol, projectController.putActualizarProyecto);
 
-// Eliminar un proyecto (Solo Admin)
 router.delete('/:id', auth.verificarToken, adminRol, projectController.deleteProyecto);
+
 
 module.exports = router;
