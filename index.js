00885990459efdabc8a8538ae0cf092552cb53d2
MGC55 <mariogc5555@gmail.com>
2025-10-08 18:49:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const config = require('./src/config.js');
 const port = config.app.port;
 
 app.listen(port, ()=> {
-    console.log(`🚀 Servidor TeamTrack escuchando en http://localhost:${port}`);
+    console.log(`Servidor TeamTrack escuchando en http://localhost:${port}`);
     console.log(`Rutas de Usuario disponibles en: http://localhost:${port}/api/users`);
 
 });
+
