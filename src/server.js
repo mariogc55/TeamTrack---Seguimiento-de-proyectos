83e10c837aaed632a1c5d361ca6893844b2c1fd2
MGC55 <mariogc5555@gmail.com>
2025-10-07 22:28:00 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,18 +5,15 @@ const cors = require('cors');
 const config = require('./config'); 
 require('./db'); 
 
-// importar rutas
 const userRoutes = require('./routes/userRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-// middlewares globales
 app.use(cors()); 
 app.use(express.json()); 
 
-// definición rutas base
 app.get('/', (req, res) => {
     res.send('Servidor TeamTrack API está ejecutándose.');
 });
@@ -25,6 +22,4 @@ app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
-
-// ¡IMPORTANTE! Exportar la app, NO iniciar el servidor aquí.
 module.exports = app; 
